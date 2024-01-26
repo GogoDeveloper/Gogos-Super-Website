647941f67593e4e4102fa2841b1e4859e9d9c64d
Gogo Dev <gogomagnusdev@gmail.com>
2024-01-26 14:04:09 +0100

    Add project files.

---
+++ Gogos-Super-Website/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Gogos_Super_Website.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
